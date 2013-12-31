afe0f4d41c669138fcdac2401660fb89f22cc622
Wonjunnn <jeong.wonjun@gmail.com>
2013-12-30 23:01:04 -0800

    refactor top-bar Jquery code to be in app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+	//Cool Nav Bar function
+  $(".sub-about").hide();
+  $(".sub-clients").hide();
+  $(".has-dropdown").hover( function(){
+    $(".header").stop().animate({"margin-top":"50px"})
+  }, function() {
+    $(".header").stop().animate({"margin-top":"0"})
+  });
+  $(".nav-clients").hover(
+    function(){
+      $(".sub-clients").stop().fadeToggle();
+      $(".sub-bar").stop().slideToggle();
+  });
+  $(".nav-about").hover(
+    function(){
+      $(".sub-about").stop().fadeToggle();
+      $(".sub-bar").stop().slideToggle();
+  });
+
 // Magical smooth scroll function.
 	$('.smooth_scroll').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
